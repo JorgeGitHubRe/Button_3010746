1354f35f4d3551b2d84c524d6ce29ea13d41269d
Jorge <Jorge@DESKTOP-5DL7PLT>
2023-08-19 18:44:01 -0600

    Agregar archivos de proyecto.

---
+++ Button_3010746/Button_3010746/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Button_3010746
+{
+    public partial class MainPage : ContentPage
+    {
+      
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Presioname de nuevo";
+        }
+
+    }
+}
